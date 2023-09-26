2294f88c56717fc639a5cff5fe5084d0ee0898c0
Junhee Kang(강준희) <121140238+KangJun-Hee@users.noreply.github.com>
2023-09-26 17:55:03 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Page from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Search/>
+        <Category />
+        <Page />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
