9994a5a90543116f0d681eb50ae10638acddd9cd
Junhee Kang(강준희) <121140238+KangJun-Hee@users.noreply.github.com>
2023-10-11 17:50:09 +0900

    desing:add images for carousel

---
+++ framer-drag/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import images from "./images";
+
+function App() {
+  console.log(images);
+  return (
+    <div className="App">
+      <motion.div className="carousel">
+        <motion.div className="inner-carousel">
+          {images.map((image) => {
+            return (
+              <motion.div className="item">
+                <img src={image} />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
---
+++ framer-drag/src/images.js
@@ -0,0 +1,8 @@
+import image1 from "./assets/3b8ca1e8-4b9d-4ef2-bd0f-5b32dec8d5e1_des-2222-hire-developer-website201-730x485.jpg";
+import image2 from "./assets/6367ddcdcaac03478d077305_62c5ecdd62c665518919f68d_Dev%20Experience.png";
+import image3 from "./assets/Azure Developer.png";
+import image4 from "./assets/CDG_blog_post_image_01.jpg";
+import image5 from "./assets/GettyImages-1332378553__2_.png";
+import image6 from "./assets/what-is-a-web-developer.jpg";
+
+export default [image1,  image3, image4, image5, image6];
