b5c62b15d31a866eda21e3f3810b2b7d0a866739
Junhee Kang(강준희) <121140238+KangJun-Hee@users.noreply.github.com>
2023-10-12 15:30:07 +0900

    style:edit carousel overflow, use dragConstraints
    done!

--- framer-drag/src/App.js
+++ framer-drag/src/App.js
@@ -4,14 +4,32 @@ import { useRef, useEffect, useState } from "react";
 import images from "./images";
 
 function App() {
-  console.log(images);
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    }, 10000);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
   return (
     <div className="App">
-      <motion.div className="carousel">
-        <motion.div className="inner-carousel">
+      <p>드래그해 좌우로 움직이세요</p>
+
+      <motion.div
+        ref={carousel}
+        className="carousel"
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
+        >
           {images.map((image) => {
             return (
-              <motion.div className="item">
+              <motion.div className="item" key={image}>
                 <img src={image} />
               </motion.div>
             );
