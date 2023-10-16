c0b8ed36c52efba7311740cdef8a7c0d8df4af91
Junhee Kang(강준희) <121140238+KangJun-Hee@users.noreply.github.com>
2023-10-16 17:47:54 +0900

    test:start making toDo

---
+++ react-todo-remake/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <header className="App">
+      <h1>Junhee's Todo List</h1>
+    </header>
+  );
+}
+
+export default App;
---
+++ react-todo-remake/components/Form.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form>
+      <input type="text" className="todo-input" />
+    </form>
+  );
+};
